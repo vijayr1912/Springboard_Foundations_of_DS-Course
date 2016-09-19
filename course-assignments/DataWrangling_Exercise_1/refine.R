3c63a594e734f97f223865eaae5e98aa4cc904cd
vijayr1912 <vijayraghunath@hotmail.com>
2016-09-19 23:21:23 +0930

    Data Wrangling assignment 1

---
+++ course-assignments/DataWrangling_Exercise_1/refine.R
@@ -0,0 +1,4 @@
+###############################################################################
+# R commands to refine the toy data set showing product purchases from an 
+# electronics store database #
+###############################################################################
