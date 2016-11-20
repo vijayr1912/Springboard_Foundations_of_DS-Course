a84c3bf9ceaff43962bdee1ab92f5914a34234ab
vijayr1912 <vijayraghunath@hotmail.com>
2016-11-20 23:02:16 +1030

    k-means clustering files

--- course-assignments/Chapter7-kMeansClustering/clustering.R
+++ course-assignments/Chapter7-kMeansClustering/clustering.R
@@ -1,73 +1,47 @@
-# This mini-project is based on the K-Means exercise from 'R in Action'
-# Go here for the original blog post and solutions
-# http://www.r-bloggers.com/k-means-clustering-from-r-in-action/
+## k-means clustering analysis
 
-# Exercise 0: Install these packages if you don't have them already
+is_installed <- function(mypkg) is.element(mypkg, installed.packages()[,1]) 
 
-# install.packages(c("cluster", "rattle","NbClust"))
+load_or_install<-function(package_names)  
+{  
+  for(package_name in package_names)  
+  {  
+    if(!is_installed(package_name))  
+    {  
+      install.packages(package_name,repos="http://lib.stat.cmu.edu/R/CRAN")  
+    }  
+    library(package_name,character.only=TRUE,quietly=TRUE,verbose=FALSE)  
+  }  
+}
+
+load_or_install(c("cluster", "rattle","NbClust"))
 
 # Now load the data and look at the first few rows
 data(wine, package="rattle")
 head(wine)
 
-# Exercise 1: Remove the first column from the data and scale
-# it using the scale() function
+## standardize data
+df <- scale(wine[-1])
 
-
-# Now we'd like to cluster the data using K-Means. 
-# How do we decide how many clusters to use if you don't know that already?
-# We'll try two methods.
-
-# Method 1: A plot of the total within-groups sums of squares against the 
-# number of clusters in a K-means solution can be helpful. A bend in the 
-# graph can suggest the appropriate number of clusters. 
-
-wssplot <- function(data, nc=15, seed=1234){
-	              wss <- (nrow(data)-1)*sum(apply(data,2,var))
-               	      for (i in 2:nc){
-		        set.seed(seed)
-	                wss[i] <- sum(kmeans(data, centers=i)$withinss)}
-	                
-		      plot(1:nc, wss, type="b", xlab="Number of Clusters",
-	                        ylab="Within groups sum of squares")
-	   }
-
+## determine number of clusters
 wssplot(df)
-
-# Exercise 2:
-#   * How many clusters does this method suggest?
-#   * Why does this method work? What's the intuition behind it?
-#   * Look at the code for wssplot() and figure out how it works
-
-# Method 2: Use the NbClust library, which runs many experiments
-# and gives a distribution of potential number of clusters.
-
 library(NbClust)
 set.seed(1234)
 nc <- NbClust(df, min.nc=2, max.nc=15, method="kmeans")
-barplot(table(nc$Best.n[1,]),
-	          xlab="Numer of Clusters", ylab="Number of Criteria",
-		            main="Number of Clusters Chosen by 26 Criteria")
+table(nc$Best.n[1,])
 
+## k means cluster analysis
+barplot(table(nc$Best.n[1,]), 
+        xlab="Numer of Clusters", ylab="Number of Criteria",
+        main="Number of Clusters Chosen by 26 Criteria")
 
-# Exercise 3: How many clusters does this method suggest?
+set.seed(1234)
+fit.km <- kmeans(df, 3, nstart=25)
+fit.km$size
 
+fit.km$centers
 
-# Exercise 4: Once you've picked the number of clusters, run k-means 
-# using this number of clusters. Output the result of calling kmeans()
-# into a variable fit.km
+aggregate(wine[-1], by=list(cluster=fit.km$cluster), mean)
 
-# fit.km <- kmeans( ... )
-
-# Now we want to evaluate how well this clustering does.
-
-# Exercise 5: using the table() function, show how the clusters in fit.km$clusters
-# compares to the actual wine types in wine$Type. Would you consider this a good
-# clustering?
-
-
-# Exercise 6:
-# * Visualize these clusters using  function clusplot() from the cluster library
-# * Would you consider this a good clustering?
-
-#clusplot( ... )
+## A cross-tabulation of Type (wine varietal) and cluster membership
+ct.km <- table(wine$Type, fit.km$cluster)
